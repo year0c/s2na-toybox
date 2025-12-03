85806d662945a9261098e647ad64e6de1230cf86
Filter <not.so.filter@hotmail.com>
2025-12-03 17:34:20 -0500

    adjustments

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -1,8 +1,8 @@
 @echo off
 call tool/md5 s2built.bin md5
 if "%md5%" equ "a460bf633579a80eebbc09d6809e1b09" (
-      echo MD5 identical!
+	echo MD5 identical!
 ) else (
-      echo MD5 does not match.
+	echo MD5 does not match.
 )
 pause
--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
+if exist s2built.bin move /y s2built.bin s2built.prev.bin >NUL
 tool\windows\asw -xx -q -A -L -U -E -i . main.asm
 tool\windows\p2bin -z=0,kosinski,Size_of_DAC_driver_guess,after main.p s2built.bin
 del main.p

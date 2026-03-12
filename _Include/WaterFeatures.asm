03316ed3985335462f99f5d0fbaf247f70d0d162
Filter <not.so.filter@hotmail.com>
2026-03-12 15:15:50 -0400

    WaterFeatures work

--- _Include/WaterFeatures.asm
+++ _Include/WaterFeatures.asm
@@ -4,7 +4,18 @@
 ChangeWaterSurfacePos:
 		tst.b	(Water_flag).w
 		beq.s	locret_403E
-		move.w	(Camera_RAM).w,d1
+		move.w	(Camera_X_pos).w,d1
+	if FixBugs
+		; This function can cause the water surface's to be cut off at the
+		; left when the game is paused. This is because this function pushes
+		; the water surface sprite to the right every frame. To fix this,
+		; just avoid pushing the sprite to the right when the game is about
+		; to be paused.
+		move.b	(v_jpadpress1).w,d0 ; is Start button pressed?
+		or.b	(v_2Pjpadpress).w,d0 ; (either player)
+		andi.b	#btnStart,d0
+		bne.s	loc_402C
+	endif
 		btst	#0,(Timer_frames+1).w
 		beq.s	loc_402C
 		addi.w	#$20,d1
@@ -30,7 +41,7 @@ WaterEffects:
 		tst.b	(Deform_lock).w
 		bne.s	loc_4058
 		cmpi.b	#6,(v_player+obRoutine).w
-		bcc.s	loc_4058
+		bhs.s	loc_4058
 		bsr.w	DynamicWaterHeight
 
 loc_4058:
@@ -42,7 +53,7 @@ loc_4058:
 		move.w	d0,(v_waterpos1).w
 		move.w	(v_waterpos1).w,d0
 		sub.w	(Camera_Y_pos).w,d0
-		bcc.s	loc_4086
+		bhs.s	loc_4086
 		tst.w	d0
 		bpl.s	loc_4086
 		move.b	#223,(v_hbla_line).w
@@ -50,7 +61,7 @@ loc_4058:
 
 loc_4086:
 		cmpi.w	#223,d0
-		bcs.s	loc_4090
+		blo.s	loc_4090
 		move.w	#223,d0
 
 loc_4090:
@@ -77,7 +88,7 @@ DynamicWaterHeight:
 		move.w	(v_waterpos3).w,d0
 		sub.w	(v_waterpos2).w,d0
 		beq.s	locret_40C6
-		bcc.s	loc_40C2
+		bhs.s	loc_40C2
 		neg.w	d1
 
 loc_40C2:
@@ -113,24 +124,24 @@ locret_40F6:
 ; ---------------------------------------------------------------------------
 
 S1DynWater_LZ1:						; leftover from Sonic 1
-		move.w	(Camera_RAM).w,d0
+		move.w	(Camera_X_pos).w,d0
 		move.b	(v_wtr_routine).w,d2
 		bne.s	loc_4164
 		move.w	#$B8,d1
 		cmpi.w	#$600,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.w	#$108,d1
 		cmpi.w	#$200,(v_player+obY).w
-		bcs.s	loc_414E
+		blo.s	loc_414E
 		cmpi.w	#$C00,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.w	#$318,d1
 		cmpi.w	#$1080,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.b	#$80,(f_switch+5).w
 		move.w	#$5C8,d1
 		cmpi.w	#$1380,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.w	#$3A8,d1
 		cmp.w	(v_waterpos2).w,d1
 		bne.s	loc_4148
@@ -143,10 +154,10 @@ loc_4148:
 
 loc_414E:
 		cmpi.w	#$C80,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.w	#$E8,d1
 		cmpi.w	#$1500,d0
-		bcs.s	loc_4148
+		blo.s	loc_4148
 		move.w	#$108,d1
 		bra.s	loc_4148
 ; ---------------------------------------------------------------------------
@@ -155,10 +166,10 @@ loc_4164:
 		subq.b	#1,d2
 		bne.s	locret_4188
 		cmpi.w	#$2E0,(v_player+obY).w
-		bcc.s	locret_4188
+		bhs.s	locret_4188
 		move.w	#$3A8,d1
 		cmpi.w	#$1300,d0
-		bcs.s	loc_4184
+		blo.s	loc_4184
 		move.w	#$108,d1
 		move.b	#2,(v_wtr_routine).w
 
@@ -170,13 +181,13 @@ locret_4188:
 ; ---------------------------------------------------------------------------
 
 DynWater_HPZ2:
-		move.w	(Camera_RAM).w,d0		; leftover from Sonic 1's LZ2
+		move.w	(Camera_X_pos).w,d0		; leftover from Sonic 1's LZ2
 		move.w	#$328,d1
 		cmpi.w	#$500,d0
-		bcs.s	loc_41A6
+		blo.s	loc_41A6
 		move.w	#$3C8,d1
 		cmpi.w	#$B00,d0
-		bcs.s	loc_41A6
+		blo.s	loc_41A6
 		move.w	#$428,d1
 
 loc_41A6:
@@ -185,16 +196,16 @@ loc_41A6:
 ; ---------------------------------------------------------------------------
 
 DynWater_HPZ3:
-		move.w	(Camera_RAM).w,d0		; Leftover from Sonic 1's LZ3
+		move.w	(Camera_X_pos).w,d0		; Leftover from Sonic 1's LZ3
 		move.b	(v_wtr_routine).w,d2
 		bne.s	loc_41F2
 		move.w	#$900,d1
 		cmpi.w	#$600,d0
-		bcs.s	loc_41E8
+		blo.s	loc_41E8
 		cmpi.w	#$3C0,(v_player+obY).w
-		bcs.s	loc_41E8
+		blo.s	loc_41E8
 		cmpi.w	#$600,(v_player+obY).w
-		bcc.s	loc_41E8
+		bhs.s	loc_41E8
 		move.w	#$4C8,d1
 		move.b	#$4B,(v_lvllayout+$206).w
 		move.b	#1,(v_wtr_routine).w
@@ -212,22 +223,22 @@ loc_41F2:
 		bne.s	loc_423C
 		move.w	#$4C8,d1
 		cmpi.w	#$770,d0
-		bcs.s	loc_4236
+		blo.s	loc_4236
 		move.w	#$308,d1
 		cmpi.w	#$1400,d0
-		bcs.s	loc_4236
+		blo.s	loc_4236
 		cmpi.w	#$508,(v_waterpos3).w
 		beq.s	loc_4222
 		cmpi.w	#$600,(v_player+obY).w
-		bcc.s	loc_4222
+		bhs.s	loc_4222
 		cmpi.w	#$280,(v_player+obY).w
-		bcc.s	loc_4236
+		bhs.s	loc_4236
 
 loc_4222:
 		move.w	#$508,d1
 		move.w	d1,(v_waterpos2).w
 		cmpi.w	#$1770,d0
-		bcs.s	loc_4236
+		blo.s	loc_4236
 		move.b	#2,(v_wtr_routine).w
 
 loc_4236:
@@ -240,10 +251,10 @@ loc_423C:
 		bne.s	loc_4266
 		move.w	#$508,d1
 		cmpi.w	#$1860,d0
-		bcs.s	loc_4260
+		blo.s	loc_4260
 		move.w	#$188,d1
 		cmpi.w	#$1AF0,d0
-		bcc.s	loc_425A
+		bhs.s	loc_425A
 		cmp.w	(v_waterpos2).w,d1
 		bne.s	loc_4260
 
@@ -260,10 +271,10 @@ loc_4266:
 		bne.s	loc_42A2
 		move.w	#$188,d1
 		cmpi.w	#$1AF0,d0
-		bcs.s	loc_4298
+		blo.s	loc_4298
 		move.w	#$900,d1
 		cmpi.w	#$1BC0,d0
-		bcs.s	loc_4298
+		blo.s	loc_4298
 		move.b	#4,(v_wtr_routine).w
 		move.w	#$608,(v_waterpos3).w
 		move.w	#$7C0,(v_waterpos2).w
@@ -279,7 +290,7 @@ loc_4298:
 
 loc_42A2:
 		cmpi.w	#$1E00,d0
-		bcs.s	locret_42AE
+		blo.s	locret_42AE
 		move.w	#$128,(v_waterpos3).w
 
 locret_42AE:
@@ -288,8 +299,8 @@ locret_42AE:
 
 DynWater_HPZ4:
 		move.w	#$228,d1			; Leftover from Sonic 1's SBZ3
-		cmpi.w	#$F00,(Camera_RAM).w
-		bcs.s	loc_42C0
+		cmpi.w	#$F00,(Camera_X_pos).w
+		blo.s	loc_42C0
 		move.w	#$4C8,d1
 
 loc_42C0:
@@ -317,14 +328,18 @@ loc_42EA:
 loc_42EE:
 		move.w	obX(a1),d0
 		cmp.w	(a2),d0
-		bcs.w	loc_438C
+		blo.w	loc_438C
 		cmp.w	4(a2),d0
-		bcc.w	loc_438C
+		bhs.w	loc_438C
 		move.w	obY(a1),d2
 		cmp.w	2(a2),d2
-		bcs.w	loc_438C
+		blo.w	loc_438C
 		cmp.w	6(a2),d2
-		bcc.s	loc_438C
+		bhs.s	loc_438C
+	if FixBugs
+		; d0 is overwritten but later used as if it wasn't!
+		move.w	d0,d1
+	endif
 		move.b	(Vint_runcount+3).w,d0
 		andi.b	#$3F,d0
 		bne.s	loc_4326
@@ -335,11 +350,15 @@ loc_4326:
 		tst.b	(f_wtunnelallow).w
 		bne.w	locret_43A2
 		cmpi.b	#4,obRoutine(a1)
-		bcc.s	loc_439E
+		bhs.s	loc_439E
 		move.b	#1,(f_wtunnelmode).w
+	if FixBugs
+		; See above.
+		move.w	d1,d0
+	endif
 		subi.w	#$80,d0
 		cmp.w	(a2),d0
-		bcc.s	loc_4354
+		bhs.s	loc_4354
 		moveq	#2,d0
 		cmpi.b	#1,(Current_Act).w
 		bne.s	loc_4350
@@ -352,7 +371,7 @@ loc_4354:
 		addi.w	#4,obX(a1)
 		move.w	#$400,obVelX(a1)
 		move.w	#0,obVelY(a1)
-		move.b	#$F,obAnim(a1)
+		move.b	#AniIDSonAni_Float2,obAnim(a1)
 		bset	#1,obStatus(a1)
 		btst	#0,(v_jpadhold1).w
 		beq.s	loc_437E
@@ -372,7 +391,7 @@ loc_438C:
 		dbf	d1,loc_42EE
 		tst.b	(f_wtunnelmode).w
 		beq.s	locret_43A2
-		move.b	#0,obAnim(a1)
+		move.b	#AniIDSonAni_Walk,obAnim(a1)
 
 loc_439E:
 		clr.b	(f_wtunnelmode).w
@@ -380,8 +399,14 @@ loc_439E:
 locret_43A2:
 		rts
 ; ---------------------------------------------------------------------------
-S1LZWind_Data:	dc.w  $A80, $300, $C10,	$380
-		dc.w  $F80, $100,$1410,	$180, $460, $400, $710,	$480, $A20, $600,$1610,	$6E0, $C80, $600,$13D0,	$680
+
+		;    left, top,  right, bottom boundaries
+S1LZWind_Data:	dc.w $A80, $300, $C10,  $380 ; act 1 values (set 1)
+		dc.w $F80, $100, $1410,	$180 ; act 1 values (set 2)
+		dc.w $460, $400, $710,  $480 ; act 2 values
+		dc.w $A20, $600, $1610, $6E0 ; act 3 values
+		dc.w $C80, $600, $13D0, $680 ; SBZ act 3 values
+		even
 ; ---------------------------------------------------------------------------
 
 S1_LZWaterSlides:
@@ -397,8 +422,8 @@ loc_43E4:
 		add.w	d1,d0
 		lea	(v_lvllayout).w,a2
 		move.b	(a2,d0.w),d0
-		lea	byte_4465(pc),a2
-		moveq	#6,d1
+		lea	Slide_Chunks_End(pc),a2
+		moveq	#Slide_Chunks_End-Slide_Chunks-1,d1
 
 loc_43F8:
 		cmp.b	-(a2),d0
@@ -417,19 +442,19 @@ locret_4410:
 
 loc_4412:
 		cmpi.w	#3,d1
-		bcc.s	loc_441A
+		bhs.s	loc_441A
 		nop
 
 loc_441A:
 		bclr	#0,obStatus(a1)
-		move.b	byte_4456(pc,d1.w),d0
+		move.b	Slide_Speeds(pc,d1.w),d0
 		move.b	d0,obInertia(a1)
 		bpl.s	loc_4430
 		bset	#0,obStatus(a1)
 
 loc_4430:
-		clr.b	$15(a1)
-		move.b	#$1B,obAnim(a1)
+		clr.b	obInertia+1(a1)
+		move.b	#AniIDSonAni_Slide,obAnim(a1)
 		move.b	#1,(f_slidemode).w
 		move.b	(Vint_runcount+3).w,d0
 		andi.b	#$1F,d0
@@ -440,5 +465,11 @@ loc_4430:
 locret_4454:
 		rts
 ; ---------------------------------------------------------------------------
-byte_4456:	dc.b  $A,$F5, $A,$F6,$F5,$F4, $B,  0,  2,  7,  3,$4C,$4B,  8,  4
-byte_4465:	dc.b 0
+Slide_Speeds:
+		dc.b 10, -11, 10, -10, -11, -12, 11
+		even
+
+Slide_Chunks:
+		dc.b 2, 7, 3, $4C, $4B, 8, 4
+Slide_Chunks_End:
+		even

1b2c22c47b21b1b408c9c4d604d153c7717266a5
Filter <not.so.filter@hotmail.com>
2026-03-12 14:59:18 -0400

    make palette cycle use palette line ram

--- _Include/PaletteCycle.asm
+++ _Include/PaletteCycle.asm
@@ -38,7 +38,7 @@ loc_1E7C:
 		addq.w	#1,(v_pcyc_num).w
 		andi.w	#3,d0
 		lsl.w	#3,d0
-		lea	(v_palette+$50).w,a1
+		lea	(v_palette_line_3+$10).w,a1
 		move.l	(a0,d0.w),(a1)+
 		move.l	4(a0,d0.w),(a1)
 
@@ -54,27 +54,27 @@ PalCycle_CPZ:
 		move.w	(v_pcyc_num).w,d0
 		addq.w	#6,(v_pcyc_num).w
 		cmpi.w	#$36,(v_pcyc_num).w
-		bcs.s	loc_1ECC
+		blo.s	loc_1ECC
 		move.w	#0,(v_pcyc_num).w
 
 loc_1ECC:
-		lea	(v_palette+$78).w,a1
+		lea	(v_palette_line_4+$18).w,a1
 		move.l	(a0,d0.w),(a1)+
 		move.w	4(a0,d0.w),(a1)
 		lea	(Pal_CPZCyc2).l,a0
 		move.w	(v_pal_buffer+2).w,d0
 		addq.w	#2,(v_pal_buffer+2).w
 		cmpi.w	#$2A,(v_pal_buffer+2).w
-		bcs.s	loc_1EF4
+		blo.s	loc_1EF4
 		move.w	#0,(v_pal_buffer+2).w
 
 loc_1EF4:
-		move.w	(a0,d0.w),(v_palette+$7E).w
+		move.w	(a0,d0.w),(v_palette_line_4+$1E).w
 		lea	(Pal_CPZCyc3).l,a0
 		move.w	(v_pal_buffer+4).w,d0
 		addq.w	#2,(v_pal_buffer+4).w
 		andi.w	#$1E,(v_pal_buffer+4).w
-		move.w	(a0,d0.w),(v_palette+$5E).w
+		move.w	(a0,d0.w),(v_palette_line_3+$1E).w
 
 locret_1F14:
 		rts
@@ -87,15 +87,15 @@ PalCycle_HPZ:
 		lea	(Pal_HPZCyc1).l,a0
 		move.w	(v_pcyc_num).w,d0
 		subq.w	#2,(v_pcyc_num).w
-		bcc.s	loc_1F38
+		bhs.s	loc_1F38
 		move.w	#6,(v_pcyc_num).w
 
 loc_1F38:
-		lea	(v_palette+$72).w,a1
+		lea	(v_palette_line_4+$12).w,a1
 		move.l	(a0,d0.w),(a1)+
 		move.l	4(a0,d0.w),(a1)
 		lea	(Pal_HPZCyc2).l,a0
-		lea	(v_palette_water+$72).w,a1
+		lea	(v_palette_water_line_4+$12).w,a1
 		move.l	(a0,d0.w),(a1)+
 		move.l	4(a0,d0.w),(a1)
 
@@ -112,8 +112,8 @@ PalCycle_EHZ:
 		addq.w	#1,(v_pcyc_num).w
 		andi.w	#3,d0
 		lsl.w	#3,d0
-		move.l	(a0,d0.w),(v_palette+$26).w
-		move.l	4(a0,d0.w),(v_palette+$3C).w
+		move.l	(a0,d0.w),(v_palette_line_2+6).w
+		move.l	4(a0,d0.w),(v_palette_line_2+$1C).w
 
 locret_1F84:
 		rts
@@ -129,8 +129,8 @@ PalCycle_HTZ:
 		andi.w	#$F,d0
 		move.b	Pal_HTZCyc2(pc,d0.w),(v_pcyc_time+1).w
 		lsl.w	#3,d0
-		move.l	(a0,d0.w),(v_palette+$26).w
-		move.l	4(a0,d0.w),(v_palette+$3C).w
+		move.l	(a0,d0.w),(v_palette_line_2+6).w
+		move.l	4(a0,d0.w),(v_palette_line_2+$1C).w
 
 locret_1FB8:
 		rts
